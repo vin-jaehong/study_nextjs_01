430d74c61d1c42d19abb9a9a5c4b8c55e5687b2c
JaeHong <dnflwoghddl@gmail.com>
2022-08-01 16:02:05 +0900

    라우팅 테스트

---
+++ pages/posts/first-posts.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function FirstPosts() {
+    return (
+        <>
+            <Link href="/">
+                <a>홈으로 돌아가기</a>
+            </Link>
+        </>
+    );
+}
