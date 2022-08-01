4a52a061c958086685c3db19320af56808b21fbc
JaeHong <dnflwoghddl@gmail.com>
2022-08-01 16:32:10 +0900

    이미지 컴포넌트 추가 테스트

--- pages/posts/first-posts.js
+++ pages/posts/first-posts.js
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FirstPosts() {
     return (
         <>
+            <Image src="/images/profile.jpg" width={200} height={200}></Image>
             <Link href="/">
                 <a>홈으로 돌아가기</a>
             </Link>
