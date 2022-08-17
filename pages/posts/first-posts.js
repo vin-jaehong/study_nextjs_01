c08c9590284f6fd7e5f40652270faaabe1dd90f0
JaeHong <dnflwoghddl@gmail.com>
2022-08-17 16:09:36 +0900

    첫번째 게시글 페이지 헤더 타이틀 태그 추가

--- pages/posts/first-posts.js
+++ pages/posts/first-posts.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPosts() {
     return (
         <>
-            <Image src="/images/profile.jpg" width={200} height={200}></Image>
-            <Link href="/">
-                <a>홈으로 돌아가기</a>
-            </Link>
+            <Head>
+                <title>첫번째 글</title>
+            </Head>
+            <h1>첫번째 글</h1>
+            <h2>
+                <Link href="/">
+                    <a>홈으로 돌아가기</a>
+                </Link>
+            </h2>
         </>
     );
 }
