40a3bb54dc83b33469ef074cb29816cf9246fbeb
JaeHong <dnflwoghddl@gmail.com>
2022-08-19 19:34:56 +0900

    외부 스크립트 로드 제거 및 vscode 깃 제어 테스트

--- pages/posts/first-posts.js
+++ pages/posts/first-posts.js
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Script from 'next/script';
 
 export default function FirstPosts() {
     return (
@@ -8,13 +7,6 @@ export default function FirstPosts() {
             <Head>
                 <title>첫번째 글</title>
             </Head>
-            <Script
-                    src="https://connect.facebook.net/en_US/sdk.js"
-                    strategy="lazyOnload"
-                    onLoad={() =>
-                    console.log(`script loaded correctly, window.FB has been populated`)
-                    }
-                />
             <h1>첫번째 글</h1>
             <h2>
                 <Link href="/">
