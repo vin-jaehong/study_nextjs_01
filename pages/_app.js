aebb6809b1ae58a1751074032f3b192e533e0fc7
JaeHong <dnflwoghddl@gmail.com>
2022-09-06 17:31:02 +0900

    js doc 주석 문구 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/global.css';
 
+/**
+ * 최상위 컴포넌트 (해당 컴포넌트 수정 시 서버 재시작 필요)
+ * @param {*} param0 
+ * @returns {*}
+ */
 export default function App({Component, pageProps}) {
     return  <Component {...pageProps} />
 }
