1ec5cf11d0bcae4878756478850e34477e310b5e
JaeHong <dnflwoghddl@gmail.com>
2022-09-30 14:57:03 +0900

    동적 경로 타이틀에 Head 컴포넌트 추가

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -22,12 +23,16 @@ export async function getStaticPaths() {
 export default function Post({postData}) {
   return (
     <Layout>
+      <Head>
         {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Head>
+      <br />
+
+      {postData.id}
+      <br />
+
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
